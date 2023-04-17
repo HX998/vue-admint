de8b91e812bb164ab09ed3608af1dd5c6fc46d14
hx_z <Zhanghx>
2023-04-17 16:29:53 +0800

    feat: feat: 调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    indent: 0,
+    'indent': 0,
     'space-before-function-paren': 0
   }
 }
